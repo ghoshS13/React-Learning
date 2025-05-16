dba5c73f1d1be5f9709f18548fce28c95cef2bf2
Shayak <shayakghosh15@gmail.com>
2025-05-16 23:33:33 +0530

    episode 2

--- App.js
+++ App.js
@@ -1,22 +1,10 @@
-// const heading = document.createElement("h1");
-// heading.innerHTML = "Hello from javascript";
-// const root = document.getElementById("root");
-// root.appendChild(heading);
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Hello From React"
-// );
-// console.log(heading); //returns object
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading); //here we are passing a object
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent = React.createElement(
   "div",
   { id: "parent" },
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am a H1 tag"),
+    React.createElement("h1", {}, "This changed"),
     React.createElement("h1", {}, "I am a H2 tag"),
   ])
 );
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Namaste React
+
+# parcel
+
+-Dev Build
+-Local Server
+-Hot module replacement
+-File watching algorithm written in C++
+-cache - faster build
+-image optimization
+-Minification
+-Bundling
+-compress
+-code splitting
+-Differntial Bundling(to support older browser)
+-Tree shaking- remove unused code
