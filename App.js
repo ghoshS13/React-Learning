ef39644da6340f089d515e076c010bd04d1b4c9a
Shayak <shayakghosh15@gmail.com>
2025-05-20 19:00:59 +0530

    episode 3

--- App.js
+++ App.js
@@ -1,12 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "This changed"),
-    React.createElement("h1", {}, "I am a H2 tag"),
-  ])
-);
+//react.createElement =>ReactElement(object)=>HTMLElement(After rendering)
+
+const heading = React.createElement("h1", { id: "heading" }, "NamAste react");
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent); //here we are passing a object
+root.render(heading);
+
+//JSX --> JSX is not HTML inside Javascript, it is JS like syntax
+//Babel transpiles it to react.createElement
+const jsxheading = (
+  <h1 className="head" tabIndex="1">
+    {" "}
+    namaste React using JSX
+  </h1>
+);
+root.render(jsxheading);
+
+//React Components
+//CLASS BASED COMPONENT(OLD WAY)
+//FUNCTIONAL COMPOMENT(NEW WAY)
+
+//React Functional Component(Normal JS function which return some jsx, first letter must be capital)
+
+const Title = () => <h1 className="head"> This is the TITLE</h1>;
+const Title2 = <h1>This is another tiltle as JS Variable</h1>;
+//component compostion
+const number = 500;
+
+const HeadingComponent = () => {
+  return (
+    <div id="container">
+      <Title />
+      {Title2}
+      <h2></h2>
+      {}
+      <h1 className="headings"> namaste React Functional component</h1>;
+    </div>
+  );
+};
+
+root.render(<HeadingComponent />);
